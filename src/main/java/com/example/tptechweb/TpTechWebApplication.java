c8ecf365e3c013682413cc7a811399dc9b2ebd17
rayan <rayanschanl@gmail.com>
2023-10-26 10:18:43 +0200

    init project

---
+++ src/main/java/com/example/tptechweb/TpTechWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.tptechweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpTechWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpTechWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tptechweb/TpTechWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tptechweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpTechWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
